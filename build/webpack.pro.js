bbdf9b4bb155fbe3f50ea019b7984242bab2588a
fengjianxiong <fengjianxiong@xin.com>
2019-05-22 10:32:18 +0800

    使用terser-webpack-plugin对js进行压缩

--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -2,6 +2,7 @@ const path =require('path')
 const merge =require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') //css代码分割
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')//css代码压缩
+const TerserPlugin=require('terser-webpack-plugin')
 const common =require('./webpack.common')
 module.exports =merge(common,{
   mode: 'production',
@@ -47,6 +48,12 @@ module.exports =merge(common,{
     })
   ],
   optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin({})],
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+      new TerserPlugin({
+        cache:true,//启用文件缓存
+        parallel:true//使用多进程并行运行来提高构建速度
+      })
+    ],
   }
 })
